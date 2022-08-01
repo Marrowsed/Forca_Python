78df55d81d875bd768a29e5d673b4bd5299eaaa4
Marrowsed <marcus.ah@gmail.com>
2022-07-31 21:28:04 -0300

    words unique

--- forca/models.py
+++ forca/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Words(models.Model):
-    word = models.CharField(max_length=200)
+    word = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return self.word
