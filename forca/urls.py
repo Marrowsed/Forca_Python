a09ec13f11d26db35950e7385805c2a293acaac6
Marrowsed <marcus.ah@gmail.com>
2022-07-31 21:29:03 -0300

    update urls

--- forca/urls.py
+++ forca/urls.py
@@ -4,6 +4,5 @@ from .views import *
 urlpatterns = [
                   path('', index, name="index"),
                   path('jogar', jogar, name="jogar"),
-                  path('jogar/gerar', generate, name="generate_db"),
                   path('jogar/regras/<str:pk>', randomize, name="randomize"),
               ]
